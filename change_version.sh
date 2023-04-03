8091dfa0470fea3914ba8674cbafa4bc6cb72e48
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2023-04-03 14:26:02 +0200

    Minor convenience improvement to `change_version.sh`

--- change_version.sh
+++ change_version.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+git_status_clean="$(set +e; git diff-index --quiet HEAD; echo $?)"
+
 find . \
     -not -path './target/*' \
     -type f \
@@ -13,3 +15,7 @@ find . \
     \;
 
 cargo +stable update -v -w
+
+if [ "${git_status_clean}" == "0" ]; then
+    git commit -pm "Version $1 release"
+fi
